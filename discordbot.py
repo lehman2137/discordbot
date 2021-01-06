963d4ed06b32309967c774ab116853039926b767
lehman2137 <77033390+lehman2137@users.noreply.github.com>
2021-01-06 10:50:36 +0900

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,25 @@
+import discord
+import os
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("똥싸는 중")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+        await message.channel.send("그래 안뇽")
+        
+        
+access_token = os.environ["BOT_TOKEN"]
+client.run("access_token")
+
+
+
